acc6b83c388b2bf834b302fcb5532291179d86b6
Matthew Thompson <supra__charged@hotmail.com>
2021-05-05 22:45:52 +0000

    add confirm to overwrite file fo fetcher.js

--- fetcher.js
+++ fetcher.js
@@ -1,10 +1,31 @@
 const fs = require('fs');
+const readline = require('readline');
 const request = require('request');
 
 const url = process.argv[2];
 const fileName = process.argv[3];
 
-if (url && fileName) {
+const doesFileAreadyExist = (file, callback) => {
+  fs.access(file, fs.constants.F_OK, (err) => {
+    if (err) {
+      callback();
+    } else {
+      const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+      });
+  
+      rl.question(`${file} already exists. Press 'Y' or 'y' overwrite, or any other key to quit.`, response => {
+        if (response === 'Y' || response === 'y') {
+          callback();
+        }
+        rl.close();
+      });
+    }
+  });
+};
+
+const makeRequest = (url, fileName) => {
   request(url, (error, response, body) => {
     if (error) {
       console.log('error:', error);
@@ -19,5 +40,11 @@ if (url && fileName) {
         console.log(`${url} body saved to ${fileName}`);
       });
     }
+  });
+};
+
+if (url && fileName) {
+  doesFileAreadyExist(fileName, () => {
+    makeRequest(url, fileName);
   });
 }
